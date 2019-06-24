71820c245210987502cea05a66cb85d4ee663d18
Dawid <nexog1997@gmail.com>
2019-06-24 17:15:45 +0200

    Bug fix v3

--- app/src/main/java/i235762/my/projekt/MainActivity.java
+++ app/src/main/java/i235762/my/projekt/MainActivity.java
@@ -1,10 +1,13 @@
 package i235762.my.projekt;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,10 +22,11 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private EditText loginForm, passwordForm;
-    private Button loginButton, newUserButton;
+    private Button loginButton, newUserButton,deleteData;
     private TextView errorTextView;
     public FirebaseAuth mAuth;
-
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
 
 
     @Override
@@ -35,8 +39,22 @@ public class MainActivity extends AppCompatActivity {
         errorTextView = findViewById(R.id.errorTextView);
         mAuth = FirebaseAuth.getInstance();
         newUserButton = findViewById(R.id.newUserButton);
-        FirebaseAuth.getInstance().signOut();
+        deleteData = findViewById(R.id.deleteData);
 
+
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+        Log.d("aaa",sharedPref.getString("email",""));
+        loginForm.setText(sharedPref.getString("email",""));
+        passwordForm.setText(sharedPref.getString("haslo",""));
+
+        FirebaseAuth.getInstance().signOut();
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            deleteData();
+            }
+        });
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,7 +106,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void logIn() {
+        editor.putString("email",loginForm.getText().toString());
+        editor.putString("haslo",passwordForm.getText().toString());
+        editor.commit();
         Intent intent = new Intent(this, NewMessagePage.class);
+
         startActivity(intent);
     }
 
@@ -103,6 +125,13 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         FirebaseAuth.getInstance().signOut();
 
+    }
+    private void deleteData(){
+        loginForm.setText("");
+        passwordForm.setText("");
+        editor.putString("email","");
+        editor.putString("haslo","");
+        editor.commit();
     }
 
 }
